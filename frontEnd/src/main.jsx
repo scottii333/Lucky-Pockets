6e3e245be7ad58a9f36c15a8bbc4d00f4cf2faaf
Scottiieee <splendidhoodies@gmail.com>
2025-06-11 02:19:06 +0800

    feat: add main entry point

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -4,11 +4,17 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Error } from "./components/Error.jsx";
+import { AutenticatedPlayer } from "./Client/AutenticatedPlayer.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/Autenticated-Player",
+    element: <AutenticatedPlayer />,
     errorElement: <Error />,
   },
 ]);
