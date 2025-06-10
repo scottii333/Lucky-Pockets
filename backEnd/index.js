5484ab42e69f138823482551274153ab78c6b229
Scottiieee <splendidhoodies@gmail.com>
2025-06-10 22:13:16 +0800

    feat: add structure for backend

---
+++ backEnd/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use("/api", () => {
+  console.log("API endpoint hit");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
