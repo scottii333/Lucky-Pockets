b09ade31760f48b5294b9e04f6d7ff0025b65de8
Scottiieee <splendidhoodies@gmail.com>
2025-06-11 16:01:12 +0800

    feat: modify bg color

--- frontEnd/src/components/Error.jsx
+++ frontEnd/src/components/Error.jsx
@@ -3,10 +3,10 @@ import { AlertTriangle } from "lucide-react";
 export const Error = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#606C38] px-4">
-      <div className="bg-white shadow-lg rounded-2xl p-6 flex items-center space-x-4">
-        <AlertTriangle className="text-yellow-500 w-8 h-8" />
+      <div className="bg-[#FEFAE0] shadow-lg rounded-2xl p-6 flex items-center space-x-4">
+        <AlertTriangle className="text-[#BC6C25] w-8 h-8" />
         <div>
-          <h2 className="text-lg font-semibold text-yellow-600">
+          <h2 className="text-lg font-semibold text-[#BC6C25]">
             Page Not Found
           </h2>
           <p className="text-sm text-gray-600">
